12b274cf44b5880fb63c1914c0239415627c9af2
Yusuke Toba <toba@ws.cs.kobe-u.ac.jp>
2015-07-09 01:28:28 +0900

    Add User following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,13 @@
 class Micropost < ActiveRecord::Base
-	belongs_to :user
-	default_scope -> { order('created_at DESC') }
-	validates :content, presence: true, length: { maximum: 140 }
-	validates :user_id, presence: true
+  belongs_to :user
+  default_scope -> { order('created_at DESC') }
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :user_id, presence: true
+
+  # 与えられたユーザーがフォローしているユーザー達のマイクロポストを返す。
+  def self.from_users_followed_by(user)
+    followed_user_ids = user.followed_user_ids
+    where("user_id IN (:followed_user_ids) OR user_id = :user_id",
+          followed_user_ids: followed_user_ids, user_id: user)
+  end
 end
